16861c8675e66793081377661180ac6e2028b3b5
Kuldeep Hajare <hajarekuldeep7@gmail.com>
2021-12-18 22:31:56 +0530

    commit

---
+++ loop/Iteration1.py
@@ -0,0 +1,26 @@
+
+def Iteration():
+      no=0
+
+      while(no<5):
+            print("Hello")
+            no=no+1
+
+
+
+def Sequence():
+      print("Hello")
+      print("Hello")
+      print("Hello")
+      print("Hello")
+      print("Hello")
+
+
+def main():
+      Sequence()
+
+
+
+
+if __name__=="__main__":
+      main()
